98c0b3417c02fae33a22db69b0c10bd22bc44706
Carolina Paez <carobeth.paez@gmail.com>
2026-03-06 12:10:54 -0300

    fix: remove unnecessary closing braces in product display test

--- cypress/e2e/api/products.cy.ts
+++ cypress/e2e/api/products.cy.ts
@@ -15,14 +15,14 @@ it('Debería mostrar solo los productos que definimos en el stub', () => {
     cy.get('.inventory_item').first().should('contain.text', 'Producto Personalizado 1')
     cy.get('.inventory_item').eq(1).should('contain.text', 'Producto Personalizado 2')
     cy.get('.inventory_item').last().should('contain.text', 'Producto Personalizado 3')
-})
-
-})
 
 // Verificar que el primer producto se llama 'Sauce Labs Backpack'
 cy.get('.inventory_item_name').first()
     .should('contain.text', 'Sauce Labs Backpack')
-    
+})
+
+})
+
 // Intercepta el POST de add-to-cart
 cy.intercept('POST', '**/add-to-cart', {
     }).as('addToCart')
